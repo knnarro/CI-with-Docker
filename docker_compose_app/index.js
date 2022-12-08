6fb8490823f6c97c04597518e2d3279de65185a1
나로 <knnarro@pirecoui-MacBookPro.local>
2022-12-09 00:39:08 +0900

    feat: node.js 앱 수정

--- docker_compose_app/index.js
+++ docker_compose_app/index.js
@@ -3,20 +3,28 @@ const redis = require("redis");
 
 // 레디스 클라이언트 생성
 const client = redis.createClient({
-    host: "redis-server", // docker-compose.yml에 명시한 컨테이너 이름
-    port: 6379
+    socket: {
+        host: "redis-server", // docker-compose.yml에 명시한 컨테이너 이름
+        port: 6379
+    }
 })
 
 const app = express();
+const options = {
+    host: "0.0.0.0",
+    port: 8080
+}
 
-// 숫자는 0부터 시작
-client.set("number", 0)
 // 루트 경로로 요청이 올 때마다 1씩 증가
-app.get('/', (req, res) => {
-    client("number", (err, number)=>{
-        client.set("number", parseInt(number)+1)
-        res.send(number+"번째 새로고침")
-    })
-    client.set("number", next_number)
+app.get('/', async (req, res) => {
+    await client.connect();
+    const number = await client.get("number")
+    if(number === null){
+        number = 0
+    }
+    res.send("숫자가 1씩 올라갑니다: "+number)
+    await client.set("number", parseInt(number) + 1)
+    await client.disconnect()
 })
-app.listen(8080).then(console.log("server is running"))
+
+app.listen(options, () => console.log("server is running"))
